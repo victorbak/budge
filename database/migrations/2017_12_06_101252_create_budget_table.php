bdfd4ec4a96c0cc4353ba2c29b85553145752f6c
victorbak <victor.bak2@gmail.com>
2017-12-06 02:40:04 -0800

    database tables up

--- database/migrations/2017_12_06_101252_create_budget_table.php
+++ database/migrations/2017_12_06_101252_create_budget_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBudgetTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('budget', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->decimal('total');
+            $table->string('pay_cycle')->default('manual');
+            $table->decimal('salary_wage')->nullable;
+            $table->string('day', 9)->nullable;
+            $table->foreign('id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('budget');
     }
 }
